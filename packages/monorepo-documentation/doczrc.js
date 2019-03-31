176b2ed367af2b8fc41d7de27eaac628607ca425
Schalk Venter <venterschalk@gmail.com>
2019-03-31 21:33:52 +0200

    Adds framework to render documentation as microsite

---
+++ packages/monorepo-documentation/doczrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  "title": "🎁 React Webapp Starters",
+  "description": "A collection of standardised, open-sourced and well-documented project starters used to bootstrap React webapps at OpenUp",
+  "files": "**/*.{md,markdown}",
+  "codeSandbox": false,
+}
